6f48e097f0a0d713faca7f078189cabd3cfb6141
alberto aguilar <faraond1@gmail.com>
2020-03-03 17:34:23 -0600

    cambio de docker

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ mongoose.Promise = global.Promise;
 
 //mongoose.connect('mongodb://192.168.99.100:32768/museum_db', { useNewUrlParser: true});
 
-var mongo_host = (process.env.MONGO_SERVICE_HOST || 'localhost' );
-var mongo_port = (process.env.MONGO_SERVICE_PORT || 27017 );
-var url = 'mongodb://'+mongo_host+':'+mongo_port+'/museum_db';
+var mongo_host = (process.env.MONGO_SERVICE_HOST || 'localhost');
+var mongo_port = (process.env.MONGO_SERVICE_PORT || 27017);
+//var url = 'mongodb://'+mongo_host+':'+mongo_port+'/museum_db';
+
+
+
+var url = 'mongodb+srv://albert:qnUo39pm3x8yahWU@museum-6hik5.gcp.mongodb.net/museum_db?retryWrites=true&w=majority';
 
 
 const options = {
@@ -25,19 +29,16 @@ const options = {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-   // autoIndex: false, // Don't build indexes
+    // autoIndex: false, // Don't build indexes
     //reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-   // reconnectInterval: 500, // Reconnect every 500ms
+    // reconnectInterval: 500, // Reconnect every 500ms
     poolSize: 10, // Maintain up to 10 socket connections
     // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
-    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-  };
+};
+
 
 mongoose.connect(url, options)
-    .then(() => console.log("La conexión a la base de datos museum_db se ha realizado correctamente"+"_"+url))
+    .then(() => console.log("La conexión a la base de datos museum_db se ha realizado correctamente" + "_" + url))
     .catch(err => console.log("Error connecting db", err + url));
 
 
@@ -46,14 +47,14 @@ mongoose.connect(url, options)
 // Setup server port
 var port = process.env.PORT || 8080;
 
-    
-        // CREAR EL SERVIDOR WEB CON NODEJS
-        app.listen(port, () => {
-            console.log("** Running ServerUser on port **" + port);
-        });
-  
 
-    
+// CREAR EL SERVIDOR WEB CON NODEJS
+app.listen(port, () => {
+    console.log("** Running ServerUser on port **" + port);
+});
+
+
+
 
 
 
